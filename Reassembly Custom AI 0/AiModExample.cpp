9279d714e088f7aeaa6a9ff4697fc0245cdf8079
Chris Barrett <akaitwo@chris.codesaru.com>
2018-05-13 15:39:29 -0700

    Add not-simple AI action list (restores AHeal).
    Simple list is a little too simple for a complete experience while
    playing Reassembly, but is *much* better for a first look at AI modding.
    Keeping both in place with an ifdef toggle.  Defaulting to not-simple so
    the mod's more fun to play out of the box.
    
    Not-simple list is from the boilerplate AI mod example.  With the
    addition of the two new actions the weapon-engine-assist mod add.

--- Reassembly Custom AI 0/AiModExample.cpp
+++ Reassembly Custom AI 0/AiModExample.cpp
@@ -11,6 +11,12 @@
     if (TYPE::supportsConfig(ai->getConfig()))      \
         ai->addAction(new TYPE(ai, __VA_ARGS__));
 
+// Toggle between 0 and 1 to use the simplified action list,
+//  that's good enough most of the time (when true).
+//  If false, use more complete action list.
+// In particular, when debugging, using fewer actions can be helpful.
+#define SIMPLE_ACTION_LIST 0
+
 
 static bool featuresMatch (uint64 features) {
     // not a turret?  No match.
@@ -151,6 +157,9 @@ void GetApiVersion(int * major, int * minor) {
     *minor = 0;
 }
 
+
+#if SIMPLE_ACTION_LIST
+
 // tournament mode AI
 bool CreateAiActions(AI* ai) {
     const AICommandConfig & config = ai->getConfig();
@@ -181,3 +190,72 @@ bool CreateAiActions(AI* ai) {
 
     return true; // we handled it; no need for default AI actions
 }
+
+#else // not SIMPLE_ACTION_LIST
+
+bool CreateAiActions(AI* ai) {
+    const AICommandConfig &         config = ai->getConfig();
+    const ECommandFlags::value_type flags = config.flags;
+
+    if (config.isMobile >= 2 && (config.flags & SerialCommand::DODGES)) {
+        ai->addActionVanilla(VANILLA_ACTION_TYPE_AVOID_WEAPON);
+    }
+
+    ai->addActionVanilla(VANILLA_ACTION_TYPE_WEAPONS);
+
+    ai->addActionVanilla(VANILLA_ACTION_TYPE_FALLBACK_TARGET);
+    ai->addActionVanilla(VANILLA_ACTION_TYPE_TARGET_ENEMY);
+    ai->addActionVanilla(VANILLA_ACTION_TYPE_AVOID_CLUSTER);
+    ai->addActionVanilla(VANILLA_ACTION_TYPE_ATTACK);
+    ai->addActionVanilla(VANILLA_ACTION_TYPE_HEALERS); // notice this isn't used by the interceptor, due to supportsConfig()
+    ai->addActionVanilla(VANILLA_ACTION_TYPE_INVESTIGATE);
+
+    if (config.features&Block::ASSEMBLER)
+    {
+        ai->addActionVanilla(VANILLA_ACTION_TYPE_HEAL);
+        if (config.flags&SerialCommand::TRACTOR_TRANSIENT) {
+            ai->addActionVanilla(VANILLA_ACTION_TYPE_SCAVENGE_WEAPON);
+        }
+        if (!config.hasFreeRes || kAIEnableNoResReproduce)
+        {
+            if (config.flags&SerialCommand::METAMORPHOSIS) {
+                ai->addActionVanilla(VANILLA_ACTION_TYPE_METAMORPHOSIS);
+            }
+            ai->addActionVanilla(VANILLA_ACTION_TYPE_BUD_REPRODUCE);
+        }
+        // else ADonate: find allies and give them resources?
+    }
+    else if (config.features&Block::REGROWER)
+    {
+        ai->addActionVanilla(VANILLA_ACTION_TYPE_HEAL);
+    }
+
+    if (config.isMobile && config.isRoot() && !config.isAttached)
+    {
+        // FIXME need to either metamorphasize or plant self
+        // FIXME very bad to overwrite to non-plant blueprint and then get planted
+        ai->addActionVanilla(VANILLA_ACTION_TYPE_PLANT_SELF);
+        ai->addActionVanilla(VANILLA_ACTION_TYPE_METAMORPHOSIS);
+    }
+
+    if (config.isMobile && !nearZero(ai->command->sb.command->destination))
+    {
+        ai->appendCommandDest(ai->command->sb.command->destination, 0.25f * kSectorSize);
+    }
+
+    if (config.isMobile &&
+        !(flags&(SerialCommand::FOLLOWER)) &&
+        !config.hasParent &&
+        (flags&SerialCommand::WANDER))
+    {
+        ai->addActionVanilla(VANILLA_ACTION_TYPE_WANDER);
+    }
+
+    // finally, add our low-priority actions to use turrets for extra forward momentum
+    ADD_ACTION(ATurretBoost_Aim);
+    ADD_ACTION(ATurretBoost_Fire);
+
+    return true; // we handled it; no need for default AI actions
+}
+
+#endif
